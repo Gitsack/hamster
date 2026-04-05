d2d803d0d9c0673fbbe36c89b3ff6ce4acefbc9a
Martin Reitschmied <martin.reitschmied@gmail.com>
2026-04-05 15:36:01 +0200

    fix: migration

--- database/migrations/1774000000001_add_high_frequency_query_indexes.ts
+++ database/migrations/1774000000001_add_high_frequency_query_indexes.ts
@@ -2,19 +2,21 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
   async up() {
-    this.schema.alterTable('movies', (table) => {
-      table.index(['monitored'])
-      table.index(['has_file'])
-    })
-    this.schema.alterTable('episodes', (table) => {
-      table.index(['requested'])
-    })
-    this.schema.alterTable('books', (table) => {
-      table.index(['requested'])
-    })
-    this.schema.alterTable('tv_shows', (table) => {
-      table.index(['monitored'])
-    })
+    this.schema.raw(
+      'CREATE INDEX IF NOT EXISTS "movies_monitored_index" ON "movies" ("monitored")'
+    )
+    this.schema.raw(
+      'CREATE INDEX IF NOT EXISTS "movies_has_file_index" ON "movies" ("has_file")'
+    )
+    this.schema.raw(
+      'CREATE INDEX IF NOT EXISTS "episodes_requested_index" ON "episodes" ("requested")'
+    )
+    this.schema.raw(
+      'CREATE INDEX IF NOT EXISTS "books_requested_index" ON "books" ("requested")'
+    )
+    this.schema.raw(
+      'CREATE INDEX IF NOT EXISTS "tv_shows_monitored_index" ON "tv_shows" ("monitored")'
+    )
   }
 
   async down() {
